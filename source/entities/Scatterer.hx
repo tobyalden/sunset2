41a5d46f052f3bd4833592bdf6a406455963230e
Toby Alden <toby.alden@gmail.com>
2019-04-23 14:08:52 -0700

    Add sprite to scatterer

--- source/entities/Scatterer.hx
+++ source/entities/Scatterer.hx
@@ -18,20 +18,23 @@ class Scatterer extends Enemy {
     public static inline var MAX_SHOT_SPEED = 0.18;
     public static inline var SHOT_ACCEL = 0.00001;
     public static inline var SHOT_ROTATION = 0.00065;
-    public static inline var HEIGHT = 24;
     public static inline var MIN_SUBROUTINE_INTERVAL = 0.2;
     public static inline var MAX_SUBROUTINE_INTERVAL = 0.8;
     public static inline var SUBROUTINE_SHOT_ACCEL = 0.000084;
+    public static inline var WIDTH = 32;
+    public static inline var HEIGHT = 32;
 
-    private var sprite:Image;
+    private var sprite:Spritemap;
     private var dropDistance:Float;
     private var dropTween:Alarm;
     private var shotTimer:Alarm;
 
     public function new(x:Float, difficulty:Float) {
         super(x, -HEIGHT, HEALTH, difficulty);
-        mask = new Hitbox(HEIGHT, HEIGHT);
-        sprite = new Image("graphics/scatterer.png");
+        mask = new Hitbox(WIDTH, HEIGHT);
+        sprite = new Spritemap("graphics/32enemies.png", 32, 32);
+        sprite.add("tv", [14, 15, 16, 17, 18, 19, 20], 5);
+        sprite.add("tvslow", [21, 22, 23, 24, 25, 26, 27], 5);
         graphic = sprite;
         dropDistance = GameScene.getEnemyYPosition();
         dropTween = new Alarm(DROP_TIME, TweenType.OneShot);
@@ -54,6 +57,16 @@ class Scatterer extends Enemy {
     }
 
     override public function update() {
+        var percent = shotTimer.percent;
+        if(dropTween.active) {
+            percent = dropTween.percent;
+        }
+        sprite.setAnimFrame(
+            Main.isSlowmo() ? "tvslow" : "tv",
+            [6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6][
+                MathUtil.ilerp(0, 12, percent)
+            ]
+        );
         y = MathUtil.lerp(
             -HEIGHT, dropDistance, Ease.sineOut(dropTween.percent)
         );
